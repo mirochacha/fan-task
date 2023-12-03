7254175852bafd725a665d9a7290542e6d0f99d5
Mitsuki Oishi <thickyheadge@gmail.com>
2023-12-04 00:44:26 +0900

    fan

---
+++ O-span/src/jospan/JOspan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jospan;
+
+/**
+ *
+ * @author admini
+ */
+public class JOspan {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        InputFrame ref = new InputFrame();
+        ref.setVisible(true);
+        ref.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ O-span/src/jospan/SubNum.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jospan;
+
+/**
+ *
+ * @author admini
+ */
+public class SubNum {
+    static String SubjectNumber;
+    static int TimeLimit;
+}
